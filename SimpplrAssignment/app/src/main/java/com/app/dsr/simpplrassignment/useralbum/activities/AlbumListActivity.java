427af6ed91d6c4f02f4dac0ce4de2b2ba1b346a0
Divya <divya.nnp@gmail.com>
2018-04-26 11:17:52 +0530

    simpplrAssignment

---
+++ SimpplrAssignment/app/src/main/java/com/app/dsr/simpplrassignment/useralbum/activities/AlbumListActivity.java
@@ -0,0 +1,33 @@
+package com.app.dsr.simpplrassignment.useralbum.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.dsr.simpplrassignment.R;
+import com.app.dsr.simpplrassignment.base.BaseNavigationDrawerActivity;
+import com.app.dsr.simpplrassignment.utils.ApiConstants;
+import com.app.dsr.simpplrassignment.utils.RestUrlsUtils;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
+public class AlbumListActivity extends BaseNavigationDrawerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album_list);
+
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(ApiConstants.CLIENT_ID, AuthenticationResponse.Type.TOKEN, RestUrlsUtils.userAuthenticationURL());
+        builder.setScopes(new String[]{"user-read-private", "streaming"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, ApiConstants.AUTH_REQUEST_CODE , request);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
---
+++ SimpplrAssignment/app/src/main/java/com/app/dsr/simpplrassignment/utils/RestUrlsUtils.java
@@ -0,0 +1,10 @@
+package com.app.dsr.simpplrassignment.utils;
+
+public class RestUrlsUtils {
+
+
+    //AuthenticationRequest
+    public static String userAuthenticationURL(){
+        return "https://simpplr/callback";//https://spotify.com/oauth2/callback";
+    }
+}
